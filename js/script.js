ef3fe66749470c1191c294a0104261d17178951d
alexstolznut <alexstolznut@hotmail.com>
2018-02-08 19:15:39 -0800

    Done

---
+++ js/script.js
@@ -0,0 +1,169 @@
+//STEP 1 
+
+//var someMonth;
+//function theMonth(){
+//    const currentMonth;
+//    var summerMonth = [];
+//    myLibraryAFunction();
+//}
+//
+
+
+////STEP 2
+//
+//var numericLiteralExpression = 2;
+//var stringLiteralExpression = "Hi";
+//var booleanLiteralExpression = true;
+//var nullLiteralExpression = null;
+
+
+//STEP 3
+//var complexVariableExpression1 = 1 + (9 * 2) - 5;
+//var name = "alex";
+//var name2 = "zak";
+//var complextariableExpression2 = name + " " + name2;
+
+
+////STEP 4
+//
+//var stringFirstName, stringlastName, stringAddress, stringCity, stringState, numberZipCode, numberyourAge, stringReferralSource, booleanMayWeContactYou; 
+
+//STEP 5
+//var stringFirstName = "Alex", stringlastName = "Stolzoff", stringAddress = "555 Boulevard Street", stringCity = "San Diego", stringState = "CA", numberZipCode = 92113, numberyourAge = 25, stringReferralSource = "LinkedIn", booleanMayWeContactYou = false; 
+//
+//var stringFirstName = "Alex"; 
+//var stringlastName = "Stolzoff";
+//var stringAddress = "555 Boulevard Street";
+//var stringCity = "San Diego";
+//var stringState = "CA";
+//var numberZipCode = 92113;
+//var numberyourAge = 25;
+//var stringReferralSource = "LinkedIn";
+//var booleanMayWeContactYou = false; 
+//var stringFirstName; 
+//var stringlastName;
+//var stringAddress;
+//var stringCity;
+//var stringState;
+//var numberZipCode;
+//var numberyourAge;
+//var stringReferralSource;
+//var booleanMayWeContactYou; 
+//stringFirstName = "Alex"; 
+//stringlastName = "Stolzoff";
+//stringAddress = "555 Boulevard Street";
+//stringCity = "San Diego";
+//stringState = "CA";
+//numberZipCode = 92113;
+//numberyourAge = 25;
+//stringReferralSource = "LinkedIn";
+//booleanMayWeContactYou = false; 
+
+//STEP 6
+//var numberString = 3 + "Hi"
+//console.log(numberString);
+//var booleanString = true + "true";
+//console.log(booleanString);
+//var numeberBoolean = 1 + true;
+//console.log(numeberBoolean);
+
+//STEP 7
+
+//var firstName = "Alex";
+//window.console.log("My name is " +  firstName + " " + lastName + ".");
+//var lastName = "Stolzoff";
+
+
+//STEP 8
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'; 
+//document.write(someString);
+
+//STEP 9
+
+//var x = null;
+//console.log(x);
+//var y;
+//console.log(y);
+
+//STEP 10
+
+//console.log(typeof "Alex");
+//console.log(typeof 1);
+//console.log(typeof true);
+//console.log(typeof new Object());
+//console.log(typeof thisVar);
+
+//STEP 11
+//var name = "Zak Ruvalcaba", thisClass="JavaScript class";
+//alert("Hello " + name + ", " + "welcome to the " + thisClass + "!");
+
+//STEP 12
+//var name = "Alex Stolzoff";
+//var thisClass="JavaScript class";
+//alert("Hello " + name + ", " + "welcome to the " + thisClass + "!");
+
+////STEP 13
+//var name = "Alex Stolzoff";
+//var course="JavaScript";
+//alert("Hello " + name + ", " + "welcome to the " + course + " class!");
+
+//STEP 14
+//var name = "Alex Stolzoff";
+//var course="JavaScript";
+//alert("Hello " + name + ". " +  "\nWelcome to the " + course + " class!");
+
+//STEP 15 
+//var name = window.prompt("What's your name?");
+//var course="JavaScript";
+//alert("Hello " + name + ". " +  "\nWelcome to the " + course + " class!");
+
+//STEP 16
+//var name = window.prompt("What's your name?");
+//var course=window.prompt("What course are you taking?");
+//alert("Hello " + name + ". " +  "\nWelcome to the " + course + " class!");
+
+//STEP 17
+//var x = 10;
+//var y = 20;
+//var sum = parseInt(x) + parseInt(y);
+//console.log(sum);
+
+//STEP 18
+//var x = 20;
+//x += 20;
+//console.log(x);
+
+//STEP 19
+//var x = 20;
+//x *= 5;
+//console.log(x);
+
+//STEP 20
+//var x = 20%3;
+//x /= 1;
+//console.log(x);
+
+//STEP 21
+
+//var x = 200, y = 200;
+//console.log(x==y);
+
+//STEP 22
+//var x = 200, y = 200;
+//console.log(x!=y);
+
+//STEP 23
+//var widget = new Object();
+//console.log(typeof widget);
+
+//STEP 24
+//var widget = new Object();
+//console.log(widget instanceof Object);
+
+//STEP 25
+
+//var widget = new Object();
+//console.log(widget in Object);
+
+
+
